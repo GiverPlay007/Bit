d5e8515a23cf1485da793df18a18b036864bb0ba
GiverPlay007 <play.giver.007@gmail.com>
2021-05-06 15:30:12 -0300

    Connect ESP8266

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -1,10 +1,28 @@
+#include <ArduinoWebsockets.h>
+#include <ESP8266WiFi.h>
+
+const char* ssid = "ssid";
+const char* password = "pass";
+
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  Serial.println("Connecting...");
+  
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connection successful") ;
+  Serial.print("ESP8266 IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop()
 {
-  Serial.write("Hello");
-  delay(2000);
 }
