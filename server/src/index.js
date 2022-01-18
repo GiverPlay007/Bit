5ccf6384d15e39286276f76f9835503d9bf1cbdf
GiverPlay007 <play.giver.007@gmail.com>
2022-01-17 22:48:30 -0300

    Added ExpressJS

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,7 @@
-console.log('S')
+const express = require('express')
+
+const server = express();
+
+server
+  .get('/', (req, res) => res.send('Oi, gatinha'))
+  .listen(3000, () => console.log('Ouvindo'));
