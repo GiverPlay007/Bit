5d5cb67ce7dbe1e849f5da9801dad3309ffd64bf
GiverPlay007 <play.giver.007@gmail.com>
2022-01-19 20:09:00 -0300

    Connect to the database

--- server/src/controllers/UsersController.js
+++ server/src/controllers/UsersController.js
@@ -1,6 +1,16 @@
+import prisma from "../database/index.js"
+
 const create = async (req, res) => {
+  const data = await prisma.user.create({
+    data: {
+      username: "giverzinho",
+      password: "a7b6f1"
+    }
+  })
+
   return res.json({
-    success: true
+    success: true,
+    data
   })
 }
 
--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,13 @@
 import "dotenv/config"
+
 import { server } from "./server.js"
 
-server.listen(process.env.PORT || 3000, () => console.log("Ligado"))
+import prisma from "./database/index.js"
+
+const start = async () => {
+  await prisma.$connect()
+  console.log("Connected to database")
+  server.listen(process.env.PORT || 3000, () => console.log("Server started"))
+}
+
+start()
