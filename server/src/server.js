2516f86a33cf08c2baf4a38f2067f1ff2886633c
GiverPlay007 <play.giver.007@gmail.com>
2022-01-20 13:12:07 -0300

    Validate JSON syntax

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,18 @@ import { routes } from "./routes.js"
 
 const server = express()
 
+const errorHandler = (error, req, res, next) => {
+  if(error instanceof SyntaxError) {
+    return res.status(400).json({ error: "Invalid JSON body" })
+  }
+
+  next()
+}
+
 server.use(express.json())
+server.use(errorHandler)
+server.use(express.urlencoded({ extended: true }))
+
 server.use(routes)
 
 export { server }
