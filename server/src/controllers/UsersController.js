1fafaf544ffed87c39d75c52929d01b99e9bb557
GiverPlay007 <play.giver.007@gmail.com>
2022-01-20 12:42:46 -0300

    Check if user is already registered

--- server/src/controllers/UsersController.js
+++ server/src/controllers/UsersController.js
@@ -3,6 +3,16 @@ import prisma from "../database/index.js"
 const create = async (req, res) => {
   const { username, password } = req.body
 
+  const registered = await prisma.user.findFirst({
+    where: {
+      username
+    }
+  })
+
+  if(registered) {
+    return res.status(409).json({ error: "User already registered" })
+  }
+
   const user = await prisma.user.create({
     data: {
       username,
