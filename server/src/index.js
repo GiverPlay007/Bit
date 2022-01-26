f4016dd66bc6bea9f55f0c25d14c78f3f120611b
GiverPlay007 <play.giver.007@gmail.com>
2022-01-26 12:29:24 -0300

    Rename

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ import "dotenv/config"
 import { server } from "./server.js"
 
 import prisma from "./database/index.js"
-import { wss } from "./websocet_server.js"
+import { wss } from "./websocket_server.js"
 
 prisma.$connect().then(() => console.log("Database connected"))
 server.listen(process.env.PORT || 3000, () => console.log("Server started"))
