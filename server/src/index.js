261324d79e4a0fb78ec6c30bfce57f649546ed57
GiverPlay007 <play.giver.007@gmail.com>
2022-01-25 23:24:52 -0300

    Remove start()

--- server/src/index.js
+++ server/src/index.js
@@ -3,15 +3,9 @@ import "dotenv/config"
 import { server } from "./server.js"
 
 import prisma from "./database/index.js"
-import { wss } from "./websocet_server.js";
+import { wss } from "./websocet_server.js"
 
-let websocketServer;
+prisma.$connect().then(() => console.log("Database connected"))
+server.listen(process.env.PORT || 3000, () => console.log("Server started"))
 
-const start = async () => {
-  await prisma.$connect();
-  console.log("Connected to database")
-  server.listen(process.env.PORT || 3000, () => console.log("Server started"))
-  websocketServer = wss(server)
-}
-
-start()
+const websocketServer = wss(server)
